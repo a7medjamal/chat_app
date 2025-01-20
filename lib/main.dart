b334143891125d77f058de7e882c822c00e94342
a7medjamal <ahmedjmal20003@gmail.com>
2025-01-20 13:42:10 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/views/login_view.dart';
 import 'package:chat_app/views/register_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
