97a638ef7f60b2bc3814ac388845f032434c2870
a7medjamal <ahmedjmal20003@gmail.com>
2025-01-20 13:31:38 +0200

    constants

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/core/constants.dart';
 import 'package:chat_app/widgets/custom_button.dart';
 import 'package:chat_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
@@ -8,79 +9,86 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff2B475E),
+      backgroundColor: kPrimaryColor,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: [
-            Image.asset('assets/images/scholar.png'),
-            Text(
-              'Chat App',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
-                fontFamily: 'pacifico',
-              ),
-            ),
             SizedBox(
-              height: 40,
+              height: 80,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text(
-                  'Sign in',
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Email',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Password',
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            CustomButton(
-              text: 'Login',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                Image.asset('assets/images/scholar.png'),
                 Text(
-                  'Don\'t have an account? ',
+                  'Chat App',
                   style: TextStyle(
+                    fontSize: 32,
                     color: Colors.white,
-                    fontSize: 16,
+                    fontFamily: 'pacifico',
                   ),
                 ),
-                GestureDetector(
-                  onTap: () => {
-                    Navigator.pushNamed(context, '/register'),
-                  },
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 16,
+                SizedBox(
+                  height: 70,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Sign in',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hintText: 'Email',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hintText: 'Password',
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomButton(
+                  text: 'Login',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => {
+                        Navigator.pushNamed(context, '/register'),
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/core/constants.dart';
 import 'package:chat_app/widgets/custom_button.dart';
 import 'package:chat_app/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
@@ -8,85 +9,90 @@ class RegisterView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff2B475E),
+      backgroundColor: kPrimaryColor,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: ListView(
           children: [
-            Image.asset('assets/images/scholar.png'),
-            Text(
-              'Chat App',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.white,
-                fontFamily: 'pacifico',
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+            Column(
               children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Image.asset('assets/images/scholar.png'),
                 Text(
-                  'Register',
+                  'Chat App',
                   style: TextStyle(
-                    fontSize: 24,
+                    fontSize: 32,
                     color: Colors.white,
+                    fontFamily: 'pacifico',
                   ),
                 ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Email',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Password',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            CustomTextField(
-              hintText: 'Confirm Password',
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            CustomButton(
-              text: 'Register',
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Already have an account? ',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
+                SizedBox(
+                  height: 40,
                 ),
-                GestureDetector(
-                  onTap: () => {
-                    Navigator.pop(context),
-                  },
-                  child: Text(
-                    'Login',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 16,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hintText: 'Email',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hintText: 'Password',
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                CustomTextField(
+                  hintText: 'Confirm Password',
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomButton(
+                  text: 'Register',
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account? ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () => {
+                        Navigator.pop(context),
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
