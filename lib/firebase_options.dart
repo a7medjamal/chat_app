269088e6c3fd7e443a74f4317cbb6fec197e9383
a7medjamal <ahmedjmal20003@gmail.com>
2025-02-03 23:04:25 +0200

    modify: firebase app link

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -16,6 +16,13 @@ class DefaultFirebaseOptions {
         return android;
       case TargetPlatform.iOS:
         return ios;
+      case TargetPlatform.macOS:
+      case TargetPlatform.windows:
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for this platform - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       default:
         throw UnsupportedError(
           'DefaultFirebaseOptions are not supported for this platform.',
@@ -23,20 +30,20 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static FirebaseOptions get android => FirebaseOptions(
-        apiKey: dotenv.env['ANDROID_API_KEY']!,
-        appId: dotenv.env['ANDROID_APP_ID']!,
-        messagingSenderId: dotenv.env['ANDROID_MESSAGING_SENDER_ID']!,
-        projectId: dotenv.env['ANDROID_PROJECT_ID']!,
-        storageBucket: dotenv.env['ANDROID_STORAGE_BUCKET']!,
-      );
+  static FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.env['API_KEY_ANDROID']!,
+    appId: dotenv.env['APP_ID_ANDROID']!,
+    messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['PROJECT_ID']!,
+    storageBucket: dotenv.env['STORAGE_BUCKET']!,
+  );
 
-  static FirebaseOptions get ios => FirebaseOptions(
-        apiKey: dotenv.env['IOS_API_KEY']!,
-        appId: dotenv.env['IOS_APP_ID']!,
-        messagingSenderId: dotenv.env['IOS_MESSAGING_SENDER_ID']!,
-        projectId: dotenv.env['IOS_PROJECT_ID']!,
-        storageBucket: dotenv.env['IOS_STORAGE_BUCKET']!,
-        iosBundleId: dotenv.env['IOS_BUNDLE_ID']!,
-      );
+  static FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.env['API_KEY_IOS']!,
+    appId: dotenv.env['APP_ID_IOS']!,
+    messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+    projectId: dotenv.env['PROJECT_ID']!,
+    storageBucket: dotenv.env['STORAGE_BUCKET']!,
+    iosBundleId: dotenv.env['IOS_BUNDLE_ID']!,
+  );
 }
